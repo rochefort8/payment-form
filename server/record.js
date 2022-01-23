68253d8856a6e7f1914ffd3f006b4a2153803aa8
Yuji Ogihara <Yuji.Ogihara@sony.com>
2022-01-23 17:32:07 +0900

    Updated google-spreadsheet package and client implementation

--- server/record.js
+++ server/record.js
@@ -9,17 +9,20 @@
 require('date-utils');
 
 const config = require('../config');
-const GoogleSpreadsheet = require('google-spreadsheet');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
 
 const credentials = JSON.parse(config.sheet.credentials);
 
 var Record = function() {}      
 
+const WORKSHEET_ID_PAYER = 0,
+	WORKSHEET_ID_FEEDBACK = 1271121605 ;
+
 Record.addPayer = function(name,graduate) {
     const sheet = new GoogleSpreadsheet(config.sheet.id);
     var date = new Date();
     var formatted = date.toFormat("YYYY/MM/DD HH24:MI");
-    return this.addRaw(sheet,0,{date: formatted, name: name, graduate: graduate});
+    return this.addRaw(sheet,WORKSHEET_ID_PAYER ,{date: formatted, name: name, graduate: graduate});
 }
 
 Record.addFeedback = function(name,graduate,star,comment) {
@@ -27,36 +30,21 @@ Record.addFeedback = function(name,graduate,star,comment) {
 
     var date = new Date();
     var formatted = date.toFormat("YYYY/MM/DD HH24:MI");
-    return this.addRaw(sheet,1,
+    return this.addRaw(sheet,WORKSHEET_ID_FEEDBACK,
 		       {date: formatted, name: name, graduate:graduate,
 			star: star, comment:comment});
 }
 
-Record.addRaw = function(sheet,number,data) {
-    sheet.useServiceAccountAuth(credentials, function(err){
-	if (err) {
-	    console.log(err);
-	    return ;
-	}
-	sheet.getInfo( function( err, sheet_info ){
-	    if (err) {
-		console.log(err);
-		return ;
-	    }
-	    var sheet1 = sheet_info.worksheets[number];
-	    if (sheet1 == null) {
-		console.log('No sheet found.') ;
-		return;
-	    }
-
+Record.addRaw = async function(sheet,number,data) {
 	
-	    sheet1.addRow(data,function(err){
-		if (err){
-		console.log(err);
-		}
-	    });
-	});
-    })
+	await sheet.useServiceAccountAuth(credentials);
+	await sheet.loadInfo();
+	const sheet1 = await sheet.sheetsById[number];
+	if (sheet1 == undefined) {
+		console.log("sheet is undefined.");
+		return
+	}
+	await sheet1.addRow(data) ;
 }
 
 module.exports = Record;
